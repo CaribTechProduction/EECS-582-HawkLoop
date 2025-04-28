7a1b0bb96206bd57c8ac62be8260d62e858830af
Sanketh345 <140121413+Sanketh345@users.noreply.github.com>
2025-04-27 20:30:40 -0500

    Updated urls.py

--- backend/hawkloop_backend/hawkloop_app/urls.py
+++ backend/hawkloop_backend/hawkloop_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('vehicles/', VehicleViewSet.as_view({'get': 'list'}), name='vehicles-list'),
     path('alerts/', AlertViewSet.as_view({'get': 'list'}), name='alerts-list'),
     # path('test-ws/', websocket_test_view, name='websocket-test'),
+    path('search/', search, name='search'),
 ]
 
